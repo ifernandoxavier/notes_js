7b85564e6513c5014fbc4691202019729be114ae
ifernandoxavier <sfexavier@gmail.com>
2023-02-09 16:01:36 -0300

    alterando pastas

---
+++ js/script.js
@@ -0,0 +1,7 @@
+console.log(document.body.childNodes[1].childNodes[3].innerText)
+console.log(document.getElementsByTagName('p')[0].innerText)
+console.log(document.getElementById('descricao').innerText)
+console.log(document.getElementsByClassName('itens'))
+
+console.log(document.querySelector('#titulo'))
+console.log(document.querySelector('.itens'))
