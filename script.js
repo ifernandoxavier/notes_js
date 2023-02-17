633be551ba85397cffc0aef02572fed0aae5820d
ifernandoxavier <sfexavier@gmail.com>
2023-02-17 12:28:45 -0300

    eventos com js

---
+++ script.js
@@ -0,0 +1,56 @@
+//colocando um evento ao botão
+// let btn = document.querySelector('#btn');
+// function msg() {
+// 	console.log("Clicou");
+// }
+// btn.addEventListener("click", msg);
+
+//removendo o elemento depois de um tempo
+// setTimeout(function () {
+// 	btn.removeEventListener("click", msg);
+// 	console.log("Removi")
+// }, 5000);
+
+// let a = document.querySelector('a');
+
+//trocando ações pré-definidas
+// a.addEventListener("click", function (e) {
+// 	e.preventDefault();
+// 	console.log("bloqueio de link");
+// })
+
+//pegando as teclas do teclado
+// window.addEventListener("keydown", function (e) {
+// 	if (e.key == "v") {
+// 		console.log("apertou a letra v");
+// 	}
+// })
+// window.addEventListener("keyup", function (e) {
+// 	if (e.key == "v") {
+// 		console.log("soltou a letra v");
+// 	}
+// })
+
+//mais com o mouse
+// btn.addEventListener("mousedown", function () {
+// 	console.log("apertou o botão");
+// });
+// btn.addEventListener("mouseup", function () {
+// 	console.log("soltou  o botão");
+// })
+
+// pegando o movimento do mouse
+// window.addEventListener('mousemove', function (e) {
+// 	console.log('horizontal: ' + e.x)
+// 	console.log('vertical: ' + e.y)
+// })
+
+//Eventos por foco
+// let input = document.querySelector("input");
+// input.addEventListener("focus", function () {
+// 	console.log("Entrou no input")
+// })
+
+// input.addEventListener("blur", function () {
+// 	console.log("Saiu do input")
+// })
